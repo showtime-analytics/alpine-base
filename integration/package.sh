ddd73827b884976e475d0499dd818e06b12a75cf
Alberto Gregoris <alberto@showtimeanalytics.com>
2016-03-09 09:23:45 +0000

    chore(integration): change package task

--- integration/package.sh
+++ integration/package.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ -z ${DOCKER_REGISTRY+x} ]; then
-    echo "Environment variable \"DOCKER_REGISTRY\" does not exist!"
-    exit 1
-fi
-
-if [ -z ${IMAGE_NAME+x} ]; then
-    echo "Environment variable \"IMAGE_NAME\" does not exist!"
-    exit 1
-fi
-
 if [ -z ${GO_PIPELINE_COUNTER+x} ]; then
     echo "Environment variable \"GO_PIPELINE_COUNTER\" does not exist!"
     exit 1
@@ -19,8 +9,6 @@ cd $(dirname $0)/..
 
 VERSION=`cat version`-${GO_PIPELINE_COUNTER}
 
-(set -x; ./scripts/package.sh $DOCKER_REGISTRY $IMAGE_NAME $VERSION)
-
-echo "${DOCKER_REGISTRY}/${IMAGE_NAME}" > repository
+(set -x; ./scripts/package.sh "" "" $VERSION)
 
 echo "${VERSION}" > build_version
