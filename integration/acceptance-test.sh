80fd0229946767f86ada9df58daa56b3e12a27a3
Alberto Gregoris <alberto@showtimeanalytics.com>
2016-03-09 09:25:03 +0000

    chore(integration): change acceptance-test task

--- integration/acceptance-test.sh
+++ integration/acceptance-test.sh
@@ -2,14 +2,9 @@
 
 cd $(dirname $0)/..
 
-if [ ! -f repository ]; then
-    echo "File \"repository\" not found!"
-    exit 1
-fi
-
 if [ ! -f build_version ]; then
     echo "File \"build_version\" not found!"
     exit 1
 fi
 
-./scripts/test.sh `cat repository` `cat build_version`
+(set -x; ./scripts/test.sh "" `cat build_version`)
